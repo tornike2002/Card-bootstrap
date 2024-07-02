997a72922da2bf11a5cae717df6c9137c37f0f8f
tornike2002 <toko.toka2000@gmail.com>
2024-07-02 18:29:41 +0400

    created first component home page and added to default route /

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
