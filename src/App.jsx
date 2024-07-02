906e9ee680faece05de5b675edc1bed393cba739
tornike2002 <toko.toka2000@gmail.com>
2024-07-02 18:32:32 +0400

    fixed unimported tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import UserInfo from "./components/User/UserInfo";
+import Cards from "./components/Cards/Cards";
 function App() {
   return (
     <>
