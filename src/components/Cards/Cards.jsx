eba59220fa7e9a21b1918d6274dc0d0be95e1be3
tornike2002 <toko.toka2000@gmail.com>
2024-07-03 16:51:01 +0400

    fixed links code

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,29 +1,88 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { useSelector } from "react-redux";
-import logo from "../../assets/logo.webp";
+import logo from "../../assets/logo.png";
+import { RiCheckboxCircleLine } from "react-icons/ri";
+import {
+  FaInstagram,
+  FaFacebookF,
+  FaTwitter,
+  FaDiscord,
+} from "react-icons/fa6";
+
 const Cards = () => {
   const users = useSelector((state) => state.users.value);
+
+  const socialMediaIcons = {
+    Instagram: <FaInstagram />,
+    Facebook: <FaFacebookF />,
+    Twitter: <FaTwitter />,
+    Discord: <FaDiscord />,
+  };
+  const getIconComponent = (url) => {
+    if (url.includes("instagram")) return socialMediaIcons.Instagram;
+    if (url.includes("facebook")) return socialMediaIcons.Facebook;
+    if (url.includes("twitter")) return socialMediaIcons.Twitter;
+    if (url.includes("discord")) return socialMediaIcons.Discord;
+    return null;
+  };
+
   return (
     <Container>
-      {users.map((user, index) => (
-        <div key={index} className="card w-25 d-flex justify-content-center align-items-center flex-column mt-5">
-          <img src={logo} alt="logo" width={120} height={120} />
-          <h2>{user.name}</h2>
-          <p>{user.profession}</p>
-          <h3>Services</h3>
-          <ul>
-            {user.services.map((service, index) => (
-              <li key={index}>{service}</li>
-            ))}
-          </ul>
-          <h3>Social Media</h3>
-          <ul>
-            {user.socialMedia.map((social, index) => (
-              <li key={index}>{social}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      <Row>
+        {users.map((user, index) => (
+          <Col
+            xs={12}
+            sm={6}
+            md={4}
+            lg={3}
+            key={index}
+            className="d-flex justify-content-center"
+          >
+            <div className="card w-100 d-flex justify-content-center align-items-center flex-column mt-5 bg-dark text-white p-3">
+              <img
+                src={logo}
+                alt="logo"
+                width={120}
+                height={120}
+                className="mt-3"
+              />
+              <div className="d-flex flex-column align-items-center justify-content-center">
+                <div className="d-flex align-items-center gap-2">
+                  <h2>{user.name}</h2>
+                  <RiCheckboxCircleLine
+                    style={{ color: "cyan", fontSize: "20px" }}
+                  />
+                </div>
+                <p>{user.profession}</p>
+              </div>
+
+              <div className="d-flex flex-column gap-1 w-100">
+                {user.services.map((service, index) => (
+                  <button
+                    className="pt-1 pb-1 rounded-2 border-0 bg-secondary text-white"
+                    key={index}
+                  >
+                    {service}
+                  </button>
+                ))}
+              </div>
+
+              <div className="d-flex gap-2 mt-3">
+                {user.socialMedia.map((social, index) => (
+                  <a
+                    href={`https://${social}`} // Assuming social contains full URLs like instagram.com/tornike_2
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    key={index}
+                  >
+                    {getIconComponent(social)}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </Col>
+        ))}
+      </Row>
     </Container>
   );
 };
